be540a17c84751e5bba322e71885e2958dc96b88
Sapient Dev <rushalichaudhari2012@gmail.com>
2025-01-25 17:58:45 +0530

    final

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,7 +33,7 @@ const Navbar = () => {
                 checked={isHost}
                 onChange={handleToggle}  
               />
-              <div className="relative w-11 h-6 bg-gray-500 peer-focus:outline-none rounded-full peer dark:bg-gray-500 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+              <div className="relative w-11 h-6 bg-gray-500 peer-focus:outline-none rounded-full peer dark:bg-gray-500 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#002D62] dark:peer-checked:bg-[#002D62]"></div>
               <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Switch to Host user</span>
             </label>
 
